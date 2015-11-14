051549c9e2b26df807e427cddd4c33f3e4be8cec
AtFortunato <athus.sm@gmail.com>
2015-11-14 00:13:25 -0200

    Update and rename .gitignore to Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+package br.edu.ifpb.pad.calculator;
+
+public class Calculator {
+	public int sum(int a, int b){
+		return a + b;
+	}
+	
+	public int subtracts(int a, int b){
+		return a - b;
+	}
+	
+	public int multiply(int a, int b){
+		return a * b;
+	}
+	
+	public int divides(int a, int b){
+		return a / b;
+	}
+}
