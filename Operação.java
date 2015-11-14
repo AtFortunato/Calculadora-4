e9e4e26f20837a4b1c3c1fe1b3cec9dfe7ade813
AtFortunato <athus.sm@gmail.com>
2015-11-14 00:16:14 -0200

    Create Operação.java

---
+++ Operação.java
@@ -0,0 +1,76 @@
+package br.edu.ifpb.pad.calculator;
+
+public class Operation {
+	private int operating1;
+	private char operator;
+	private int operating2;
+	private boolean free;
+	
+	public Operation(){
+		free = true;
+	}
+	
+	public int getOperating1() {
+		return operating1;
+	}
+
+	public void setOperating1(int operating1) {
+		this.operating1 = operating1;
+	}
+
+	public char getOperator() {
+		return operator;
+	}
+
+	public void setOperator(char operator) {
+		this.operator = operator;
+	}
+
+	public int getOperating2() {
+		return operating2;
+	}
+
+	public void setOperating2(int operating2) {
+		this.operating2 = operating2;
+	}
+
+	/*Synchronized Method:
+	 * This method sets a operation if has no a pendant 
+	 * operation to be processed*/
+	
+	public synchronized void setOperation(int operating1, char operator, int operating2) {
+		while(!free){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		setOperating1(operating1);
+		setOperator(operator);
+		setOperating2(operating2);
+		
+		free = false;
+		notifyAll();
+	}
+	
+	/*Synchronized Method:
+	 * This method returns a operation if has no a pendant 
+	 * operation to be processed*/
+	
+	public synchronized Operation getOperation() {
+		while(free){
+			try{
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		free = true;
+		notifyAll();
+		
+		return this;
+	}
+}
